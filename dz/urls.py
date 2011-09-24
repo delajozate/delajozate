548d4e46f9514a5cc5a2a6994d77cc6105becd08
Marko Samastur <markos@gaivo.net>
2011-09-24 18:07:50 +0200

    Home page - semi-staticen

---
+++ dz/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('dz.views',
+    url(r'^poslanec/(?P<slug>[A-Za-z0-9-_]{96})/$', 'poslanec', name='poslanec'),
+    url(r'^$', 'home', name='home'),
+)
--- dz/views.py
+++ dz/views.py
@@ -1,1 +1,80 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, Context, loader
+from django.core.cache import cache
+
+from models import Mandat, Poslanec
+
+import random, datetime
+
+#LONG_LIVE = 60*60*24 # Cache for a day
+LONG_LIVE = 5 # 5 seconds cache
+
+def home(request):
+	ctx = {}
+	template = loader.get_template('home.html')
+	today = datetime.date.today()
+
+	mandat = None
+	mandat_st = cache.get('dz-aktualni-mandat', 0)
+	if not mandat:
+		mandat = Mandat.objects.order_by('-st')[0]
+		mandat_st = mandat.st
+		cache.set('dz-zadnji-mandat', mandat_st, LONG_LIVE)
+
+	# Malo poslancev na mandat, zato poberi vse za random izbor
+	poslanci = cache.get('dz-poslanci', [])
+	if not poslanci:
+		if not mandat:
+			mandat = Mandat.objects.filter(st=mandat_st)[0]
+		#poslanci = Poslanec.objects.filter(mandat=mandat,do__isnull=True).values('id', 'oseba')
+		poslanci = Poslanec.objects.filter(mandat=mandat)
+		cache.set('dz-poslanci', poslanci)
+
+	# Izberi 4 nakljucne
+	st_poslancev = len(poslanci)
+	picked = []
+	izbrani = []
+	for i in range(4):
+		picked.append(poslanci[random.randint(0, st_poslancev-1)])
+
+	# Za vsakega kandidata poberi...
+	for k in picked:
+		kandidat = {}
+
+		# ... osebne podatke (ime, sliko)
+		oseba = k.oseba
+		kandidat['ime'] = "%s %s" % (oseba.ime, oseba.priimek)
+		kandidat['slika'] = oseba.slika
+
+		# ... mandate v katerih je bil (=> izracunaj stevilo in obdobje v dneh)
+		k_mandati = Poslanec.objects.filter(oseba=oseba)
+
+		kandidat['st_mandatov'] = len(k_mandati)
+		dolzina_sluzenja = sum([ ((m.do or today) - m.od).days for m in k_mandati ])
+		kandidat['dolzina_sluzenja'] = dolzina_sluzenja
+
+		'''
+		# ... stranko v kateri je
+
+		# ... stevilo strank v katerih je bil
+
+		# ... stevilo odborov
+
+		# ... stevilo komisij
+
+		# ... stevilo delegacij
+		'''
+		izbrani.append(kandidat)
+
+	ctx['izbrani'] = izbrani
+
+	return HttpResponse(template.render(Context(ctx)))
+
+
+def poslanec(request, slug):
+	# Poberi poslanca s tem slugom
+
+	# Poberi podatke zanj
+
+	# Izpisi template 
+	pass
