0bc370e6ef44330a692e1773f6333dcecef41289
Jernej Virag <jernej@virag.si>
2011-10-02 21:32:31 +0200

    Dodane osebe, stranke, skupine in odbori v iskalni indeks. Popravljena shema.

--- search/search_indexes.py
+++ search/search_indexes.py
@@ -2,7 +2,31 @@
 import haystack
 from haystack.fields import CharField, DateField, IntegerField
 from haystack.sites import site
+from dz.models import Oseba, Stranka, Odbor, Skupina
 from search.models import Zapis, Seja
+
+class OsebeIndex(haystack.indexes.SearchIndex):
+    rojstni_dan = DateField(null=True, model_attr='rojstni_dan')
+    text = CharField(document=True, use_template=True, template_name='search/indexes/osebe.txt')
+
+    def prepare(self, obj):
+        data = super(OsebeIndex, self).prepare(obj)
+        data['boost'] = 1.2     # Osebe imajo dodaten boost pri iskanju da pridejo na vrh
+        return data
+
+class StrankeIndex(haystack.indexes.SearchIndex):
+    text = CharField(document=True, use_template=True, template_name='search/indexes/stranke.txt')
+
+    def prepare(self, obj):
+        data = super(StrankeIndex, self).prepare(obj)
+        data['boost'] = 1.2
+        return data
+
+class OdboriIndex(haystack.indexes.SearchIndex):
+    text = CharField(document=True, use_template=True, template_name='search/indexes/odbori.txt')
+
+class SkupineIndex(haystack.indexes.SearchIndex):
+    text = CharField(document=True, use_template=True, template_name='search/indexes/skupine.txt')
 
 # Glavna uporaba je za iskanje sej, kjer se je celotna seja vrtela okoli iskane tematike
 # (Besedilo ni razdrobljeno)
@@ -19,5 +43,9 @@ class ZasedanjaIndex(haystack.indexes.SearchIndex):
     datum = DateField(model_attr='zasedanje__datum')
     text = CharField(document=True, use_template=True, template_name='search/indexes/zasedanja.txt')
 
+site.register(Oseba, OsebeIndex)
+site.register(Stranka, StrankeIndex)
+site.register(Odbor, OdboriIndex)
+site.register(Skupina, SkupineIndex)
 site.register(Zapis, ZasedanjaIndex)
 site.register(Seja, SejeIndex)
