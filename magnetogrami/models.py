db51cac3dffa338618c6310743c1010b1a5ba9fb
Gasper Zejn <zejn@kiberpipa.org>
2011-12-17 21:39:01 +0100

    Change spaces -> tabs.

--- magnetogrami/models.py
+++ magnetogrami/models.py
@@ -3,56 +3,56 @@ from django.db import models
 from delajozate.dz.models import Oseba
 
 class Seja(models.Model):
-    naslov = models.CharField(max_length=255)
-    seja = models.CharField(max_length=255) # TODO: check validity
-    slug = models.CharField(max_length=100, db_index=True)
-    datum_zacetka = models.DateField(null=True)
-    status = models.CharField(max_length=128)
-    mandat = models.IntegerField()
-    url = models.URLField()
+	naslov = models.CharField(max_length=255)
+	seja = models.CharField(max_length=255) # TODO: check validity
+	slug = models.CharField(max_length=100, db_index=True)
+	datum_zacetka = models.DateField(null=True)
+	status = models.CharField(max_length=128)
+	mandat = models.IntegerField()
+	url = models.URLField()
 
-    class Meta:
-        ordering = ('-mandat', '-datum_zacetka',)
+	class Meta:
+		ordering = ('-mandat', '-datum_zacetka',)
 
-    def __unicode__(self):
-        return self.naslov
+	def __unicode__(self):
+		return self.naslov
 
-    def magnetogrami(self):
-        return Zasedanje.objects.filter(seja=self).filter(models.Q(tip='magnetogram') | models.Q(tip='dobesednizapis')).select_related('seja')
+	def magnetogrami(self):
+		return Zasedanje.objects.filter(seja=self).filter(models.Q(tip='magnetogram') | models.Q(tip='dobesednizapis')).select_related('seja')
 
 class SejaInfo(models.Model):
-    seja = models.ForeignKey(Seja)
-    url = models.URLField()
-    naslov = models.CharField(max_length=255)
-    datum = models.DateField()
+	seja = models.ForeignKey(Seja)
+	url = models.URLField()
+	naslov = models.CharField(max_length=255)
+	datum = models.DateField()
 
 class Zasedanje(models.Model):
-    seja = models.ForeignKey(Seja)
-    naslov = models.CharField(max_length=255, null=True)
-    datum = models.DateField(null=True)
-    zacetek = models.TimeField(null=True)
-    konec = models.TimeField(null=True)
-    url = models.URLField(null=True)
-    tip = models.CharField(max_length=255, null=True)
+	seja = models.ForeignKey(Seja)
+	naslov = models.CharField(max_length=255, null=True)
+	datum = models.DateField(null=True)
+	zacetek = models.TimeField(null=True)
+	konec = models.TimeField(null=True)
+	url = models.URLField(null=True)
+	tip = models.CharField(max_length=255, null=True)
 
-    class Meta:
-        ordering = ('datum',)
+	class Meta:
+		ordering = ('datum',)
 
-    def __unicode__(self):
-        return self.naslov + "(" + str(self.datum) + ")"
+	def __unicode__(self):
+		return self.naslov + "(" + str(self.datum) + ")"
 
 class Zapis(models.Model):
-    zasedanje = models.ForeignKey(Zasedanje)
-    seq = models.IntegerField()
-    odstavki = models.TextField(null=True)
-    govorec = models.CharField(max_length=255, null=True)
-    govorec_oseba = models.ForeignKey(Oseba, null=True)
+	zasedanje = models.ForeignKey(Zasedanje)
+	seq = models.IntegerField()
+	odstavki = models.TextField(null=True)
+	govorec = models.CharField(max_length=255, null=True)
+	govorec_oseba = models.ForeignKey(Oseba, null=True)
 
-    class Meta:
-        ordering = ('seq',)
+	class Meta:
+		ordering = ('seq',)
 
-    def __unicode__(self):
-        if self.govorec:
-            return self.govorec + ":" + self.odstavki[0:30]
-        else:
-            return self.odstavki[0:30]
+	def __unicode__(self):
+		if self.govorec:
+			return self.govorec + ":" + self.odstavki[0:30]
+		else:
+			return self.odstavki[0:30]
--- magnetogrami/views.py
+++ magnetogrami/views.py
@@ -17,26 +17,26 @@ def seja_list(request):
 	return render_to_response('magnetogrami/seja_list.html', RequestContext(request, context))
 
 def seja(request, mdt, mandat, slug, datum_zasedanja=None):
-    assert mdt == 'dz' # for now
-    seja = Seja.objects.get(mandat=mandat, slug=slug)
-    
-    if datum_zasedanja is None:
-        try:
-            zasedanje = Zasedanje.objects.filter(
-                Q(seja=seja, tip='dobesednizapis') |
-                Q(seja=seja, tip='magnetogram')).order_by('datum')[0]
-        except IndexError:
-            zasedanje = None
-            zapisi = []
-    else:
-        zasedanje = Zasedanje.objects.filter(seja=seja, datum=datum_zasedanja).filter(Q(tip='dobesednizapis')|Q(tip='magnetogram')).select_related('zapis')
-    
-    if zasedanje is not None:
-        zapisi = Zapis.objects.filter(zasedanje=zasedanje).select_related('govorec_oseba')
-    
-    context = {
-        'seja': seja,
-        'zasedanje': zasedanje,
-        'zapisi': zapisi,
-        }
-    return render_to_response("magnetogrami/seja.html", RequestContext(request, context))
+	assert mdt == 'dz' # for now
+	seja = Seja.objects.get(mandat=mandat, slug=slug)
+	
+	if datum_zasedanja is None:
+		try:
+			zasedanje = Zasedanje.objects.filter(
+				Q(seja=seja, tip='dobesednizapis') |
+				Q(seja=seja, tip='magnetogram')).order_by('datum')[0]
+		except IndexError:
+			zasedanje = None
+			zapisi = []
+	else:
+		zasedanje = Zasedanje.objects.filter(seja=seja, datum=datum_zasedanja).filter(Q(tip='dobesednizapis')|Q(tip='magnetogram')).select_related('zapis')
+	
+	if zasedanje is not None:
+		zapisi = Zapis.objects.filter(zasedanje=zasedanje).select_related('govorec_oseba')
+	
+	context = {
+		'seja': seja,
+		'zasedanje': zasedanje,
+		'zapisi': zapisi,
+		}
+	return render_to_response("magnetogrami/seja.html", RequestContext(request, context))
