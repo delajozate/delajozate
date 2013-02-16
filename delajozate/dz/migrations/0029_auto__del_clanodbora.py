a5627aa68b2525fa57a5a7ed4d0aabbf78ae6d47
Gasper Zejn <zejn@kiberpipa.org>
2013-02-16 06:00:43 +0100

    Add forgotten migrations.

---
+++ delajozate/dz/migrations/0029_auto__del_clanodbora.py
@@ -0,0 +1,150 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Deleting model 'ClanOdbora'
+        db.delete_table('dz_clanodbora')
+
+
+    def backwards(self, orm):
+        
+        # Adding model 'ClanOdbora'
+        db.create_table('dz_clanodbora', (
+            ('podatki_preverjeni', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('do', self.gf('django.db.models.fields.DateField')(blank=True)),
+            ('odbor', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['dz.DelovnoTelo'])),
+            ('opombe', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('mandat', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['dz.Mandat'])),
+            ('funkcija', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('od', self.gf('django.db.models.fields.DateField')()),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('poslanec', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['dz.Funkcija'])),
+        ))
+        db.send_create_signal('dz', ['ClanOdbora'])
+
+
+    models = {
+        'dz.delovnotelo': {
+            'Meta': {'object_name': 'DelovnoTelo'},
+            'do': ('django.db.models.fields.DateField', [], {'blank': 'True'}),
+            'dz_id': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ime': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'mandat': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Mandat']"}),
+            'od': ('django.db.models.fields.DateField', [], {}),
+            'opombe': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'organizacija': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['dz.Organizacija']", 'unique': 'True', 'null': 'True'}),
+            'podatki_preverjeni': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'url': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '200', 'blank': 'True'})
+        },
+        'dz.drzavnizbor': {
+            'Meta': {'object_name': 'DrzavniZbor'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mandat': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Mandat']"}),
+            'organizacija': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['dz.Organizacija']", 'unique': 'True', 'null': 'True'})
+        },
+        'dz.funkcija': {
+            'Meta': {'ordering': "('id',)", 'object_name': 'Funkcija'},
+            'do': ('django.db.models.fields.DateField', [], {'blank': 'True'}),
+            'funkcija': ('django.db.models.fields.CharField', [], {'default': "'poslanec'", 'max_length': '64'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mandat': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Mandat']"}),
+            'od': ('django.db.models.fields.DateField', [], {}),
+            'opombe': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'oseba': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Oseba']"}),
+            'podatki_preverjeni': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'dz.imestranke': {
+            'Meta': {'ordering': "['-od']", 'object_name': 'ImeStranke'},
+            'do': ('django.db.models.fields.DateField', [], {'default': 'datetime.date(9999, 12, 31)', 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ime': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'od': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'stranka': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Stranka']"})
+        },
+        'dz.mandat': {
+            'Meta': {'object_name': 'Mandat'},
+            'do': ('django.db.models.fields.DateField', [], {'default': 'datetime.date(9999, 12, 31)', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'od': ('django.db.models.fields.DateField', [], {}),
+            'st': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'dz.organizacija': {
+            'Meta': {'object_name': 'Organizacija'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tip': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'dz.oseba': {
+            'Meta': {'ordering': "('priimek', 'ime')", 'object_name': 'Oseba'},
+            'dan_smrti': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '64', 'blank': 'True'}),
+            'facebook': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ime': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'opombe': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'podatki_preverjeni': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'priimek': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'rojstni_dan': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'slika': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '96', 'db_index': 'True'}),
+            'spletna_stran': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'twitter': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
+            'vir_slike': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'dz.pozicija': {
+            'Meta': {'object_name': 'Pozicija'},
+            'do': ('django.db.models.fields.DateField', [], {'default': 'datetime.date(9999, 12, 31)', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'od': ('django.db.models.fields.DateField', [], {}),
+            'opombe': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'organizacija': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Organizacija']", 'null': 'True'}),
+            'oseba': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Oseba']"}),
+            'podatki_preverjeni': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'tip': ('django.db.models.fields.CharField', [], {'default': "'poslanec'", 'max_length': '64'})
+        },
+        'dz.skupina': {
+            'Meta': {'object_name': 'Skupina'},
+            'do': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ime': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'mandat': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Mandat']"}),
+            'od': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'organizacija': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['dz.Organizacija']", 'unique': 'True', 'null': 'True'}),
+            'stranka': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Stranka']", 'null': 'True', 'blank': 'True'})
+        },
+        'dz.stranka': {
+            'Meta': {'ordering': "('-do', 'ime')", 'object_name': 'Stranka'},
+            'barva': ('django.db.models.fields.CharField', [], {'max_length': '6', 'blank': 'True'}),
+            'davcna': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
+            'do': ('django.db.models.fields.DateField', [], {'default': 'datetime.date(9999, 12, 31)', 'null': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '64', 'blank': 'True'}),
+            'facebook': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ime': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'maticna': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
+            'nastala_iz': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'spremenila_v'", 'blank': 'True', 'to': "orm['dz.Stranka']"}),
+            'od': ('django.db.models.fields.DateField', [], {'null': 'True'}),
+            'okrajsava': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
+            'opombe': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'organizacija': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['dz.Organizacija']", 'unique': 'True', 'null': 'True'}),
+            'podatki_preverjeni': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'spletna_stran': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'twitter': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'})
+        },
+        'dz.tweet': {
+            'Meta': {'ordering': "('-created_at',)", 'object_name': 'Tweet'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'oseba': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dz.Oseba']"}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'tweet_id': ('django.db.models.fields.BigIntegerField', [], {'null': 'True'})
+        }
+    }
+
+    complete_apps = ['dz']
