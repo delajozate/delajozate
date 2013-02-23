4588cb7224e4a980dab8129e2dd641afd47a1a90
Marko Samastur <markos@gaivo.net>
2013-02-23 10:24:00 +0100

    Fix display of dates on MP's page

--- delajozate/dz/models.py
+++ delajozate/dz/models.py
@@ -29,18 +29,18 @@ ORG_CHOICES = (
 
 class Organizacija(models.Model):
 	tip = models.IntegerField(choices=ORG_CHOICES)
-	
+
 	def type(self):
 		return self.get_tip_display()
-	
+
 	def __unicode__(self):
 		t = self.type()
 		return u"[%s] %s" % (t, getattr(self, t)) if t != "" else None
-	
+
 	def value(self):
 		return getattr(self, self.type())
-	
-	
+
+
 
 class Oseba(models.Model):
 	ime = models.CharField(max_length=32)
@@ -56,34 +56,34 @@ class Oseba(models.Model):
 	facebook = models.URLField(blank=True)
 	podatki_preverjeni = models.BooleanField(default=False)
 	opombe = models.TextField(blank=True)
-	
+
 	class Meta:
 		ordering = ('priimek', 'ime')
 		verbose_name_plural = u'Osebe'
-	
+
 	def __unicode__(self):
 		return u'%s %s' % (self.ime, self.priimek)
-	
+
 	def slika_or_default(self):
 		if self.slika:
 			return self.slika
 		return '/static/img/unknown.png'
-	
+
 	def slika_vir(self):
 		if self.slika:
 			return self.vir_slike
 		return 'http://www.flickr.com/photos/marypaulose/295058238/sizes/z/in/photostream/'
-	
+
 	def st_mandatov(self):
 		return self.pozicija_set.filter(tip='poslanec').count()
-	
+
 	def poslanskih_dni(self):
 		cur = connection.cursor()
 		sql = '''SELECT SUM(CASE WHEN F.do > NOW()::date THEN NOW()::date ELSE F.do END - F.od) AS st_dni FROM dz_pozicija F WHERE oseba_id = %s AND tip = 'poslanec' '''
 		params = [self.pk]
 		cur.execute(sql, params)
 		return cur.fetchall()[0][0]
-	
+
 	def clanstvo(self, day=None):
 		if day:
 			# take from dz_extras.py - datum_filter, unify me
@@ -93,15 +93,15 @@ class Oseba(models.Model):
 				Q(od__lte=high, do__gt=high) | # crosses upper boundary
 				Q(od__lte=low, do__gt=high)).filter( organizacija__stranka__gt=0).select_related('organizacija', 'organizacija__stranka'))  # or is in between
 			return clanstvo
-		
+
 		return self.pozicija_set.filter(organizacija__stranka__gt=0).order_by('-do').select_related('organizacija', 'organizacija__stranka')
-	
+
 	def funkcije(self):
 		return self.pozicija_set.exclude(organizacija=None).order_by('-do').select_related('organizacija', 'organizacija__drzavnizbor')
-	
+
 	def delovna_telesa(self):
 		return self.pozicija_set.filter(organizacija__delovnotelo__gt=0).order_by('od').select_related('organizacija', 'organizacija__delovnotelo')
-	
+
 	def save(self, *args, **kwargs):
 		if not self.slug:
 			self.slug = slug = slugify("%s %s" % (self.ime, self.priimek))
@@ -110,20 +110,20 @@ class Oseba(models.Model):
 				self.slug = "%s-%d" % (slug, count)
 				count += 1
 		super(Oseba, self).save(*args, **kwargs)
-	
+
 	def display(self):
 		return u'%s %s' % (self.ime, self.priimek)
-	
+
 
 class Tweet(models.Model):
 	tweet_id = models.BigIntegerField(blank=False, null=True)
 	text = models.CharField(max_length=250)
 	oseba = models.ForeignKey(Oseba)
 	created_at = models.DateTimeField()
-	
+
 	class Meta:
 		ordering = ('-created_at',)
-	
+
 	def __unicode__(self):
 		return self.text
 
@@ -144,48 +144,48 @@ class Stranka(models.Model):
 	podatki_preverjeni = models.BooleanField(default=False)
 	opombe = models.TextField(blank=True)
 	organizacija = models.OneToOneField(Organizacija, null=True)
-	
+
 	def save(self, *args, **kwargs):
 		if not self.organizacija_id:
 			self.organizacija = Organizacija.objects.create()
 		super(Stranka, self).save(*args, **kwargs)
-	
+
 	class Meta:
 		verbose_name_plural = u'Stranke'
 		ordering = ('-do', 'ime')
-	
+
 	def __unicode__(self):
 		return u'%s (%s)%s' % (self.ime, self.okrajsava, self.do != END_OF_TIME and u'\u271d' or u'')
-	
+
 	def display(self):
 		return u"%s" % self.ime
-	
+
 
 class ImeStranke(models.Model):
 	stranka = models.ForeignKey(Stranka)
 	ime = models.CharField(max_length=128)
 	od = models.DateField(null=True)
 	do = models.DateField(null=True, default=END_OF_TIME)
-	
+
 	class Meta:
 		ordering = [ '-od']
 		verbose_name_plural = u'Imena strank'
-	
+
 
 class Mandat(models.Model):
 	st = models.IntegerField() # Kateri mandat
 	od = models.DateField()
 	do = models.DateField(blank=True, default=END_OF_TIME)
-	
+
 	class Meta:
 		verbose_name_plural = u'Mandati'
-	
+
 	def __unicode__(self):
 		return unicode(self.st)
-	
+
 	def display(self):
 		return u"%d mandat: %s - %s" % (self.st, self.od, self.do if self.do else "")
-	
+
 
 class Skupina(models.Model): # Poslanska
 	ime = models.CharField(max_length=64)
@@ -194,34 +194,34 @@ class Skupina(models.Model): # Poslanska
 	do = models.DateField(null=True, blank=True)
 	mandat = models.ForeignKey(Mandat)
 	organizacija = models.OneToOneField(Organizacija, null=True)
-	
+
 	def save(self, *args, **kwargs):
 		if not self.organizacija_id:
 			self.organizacija = Organizacija.objects.create()
 		super(Skupina, self).save(*args, **kwargs)
-	
+
 	class Meta:
 		verbose_name_plural = u'Skupine'
-	
+
 	def display(self):
 		return ""
 
 class DrzavniZbor(models.Model):
 	mandat = models.ForeignKey(Mandat)
 	organizacija = models.OneToOneField(Organizacija, null=True)
-	
+
 	def save(self, *args, **kwargs):
 		if not self.organizacija_id:
 			self.organizacija = Organizacija.objects.create()
 		super(DrzavniZbor, self).save(*args, **kwargs)
-	
+
 	class Meta:
 		verbose_name = u'Državni Zbor'
 		verbose_name_plural = u'Državni Zbori'
-	
+
 	def display(self):
 		return u"%d. mandat Državnega zbora" % self.mandat.st
-	
+
 
 class DelovnoTelo(models.Model):
 	ime = models.CharField(max_length=2000)
@@ -233,19 +233,19 @@ class DelovnoTelo(models.Model):
 	podatki_preverjeni = models.BooleanField(default=False)
 	opombe = models.TextField(blank=True)
 	organizacija = models.OneToOneField(Organizacija, null=True)
-	
+
 	def save(self, *args, **kwargs):
 		if not self.organizacija_id:
 			self.organizacija = Organizacija.objects.create()
 		super(DelovnoTelo, self).save(*args, **kwargs)
-	
+
 	class Meta:
 		verbose_name = u'Delovno telo'
 		verbose_name_plural = u'Delovna telesa'
-	
+
 	def __unicode__(self):
 		return '%s (%s)' % (self.ime, self.mandat)
-	
+
 	def display(self):
 		return u"%s" % self.ime
 
@@ -257,10 +257,12 @@ class Pozicija(models.Model):
 	do = models.DateField(blank=True, default=END_OF_TIME)
 	podatki_preverjeni = models.BooleanField(default=False)
 	opombe = models.TextField(blank=True)
-	
+
 	class Meta:
 		verbose_name = u'Pozicija'
 		verbose_name_plural = u'Pozicije'
-	
+
 	def trajanje(self):
-		return u"%s - %s" % (self.od, self.do if self.do != END_OF_TIME else "")
+		od = self.od.strftime("%d.%m.%Y")
+		do = self.do.strftime("%d.%m.%Y") if self.do != END_OF_TIME else ""
+		return u"%s - %s" % (od, do)
