fe8056ffcff000a544308f3d556ffbb277b6aa65
Smotko <ensmotko@gmail.com>
2013-02-24 12:20:31 +0100

    Actual icalendar data

--- delajozate/cal/views.py
+++ delajozate/cal/views.py
@@ -1,28 +1,32 @@
-from icalendar import Calendar, Event
-from datetime import datetime
+from icalendar import Calendar, Event as IEvent
+import datetime
 from django.http import HttpResponse
 from datetime import date
+from models import Event
+
+NUM_EVENTS = 50
 
 def ical(request):
 
     cal = Calendar()
     
-    cal.add('prodid', '-//%s Events Calendar//%s//' % ("NAME", "DOMAIN"))
+    cal.add('prodid', '-//%s Events Calendar//%s//' % ("URNIK", "DELAJOZATE"))
     cal.add('version', '2.0')
 
-    site_token = "TOKEN"
+    site_token = "DELAJOZATE"
+    
+    events = Event.objects.order_by('start').reverse()[:NUM_EVENTS]
+    for e in events:
 
-    ical_event = Event()
-    ical_event.add('summary', "SUMM")
-    ical_event.add('dtstart', date.today())
-    ical_event.add('dtend', date.today())
-    ical_event.add('dtstamp', date.today())
-    ical_event['uid'] = '%d.event.events.%s' % (123, site_token)
-    cal.add_component(ical_event)
-    cal.add_component(ical_event)
-    cal.add_component(ical_event)
-
+        ical_event = IEvent()
+        ical_event.add('summary', e.title)
+        ical_event.add('dtstart', e.start)
+        ical_event.add('dtend', e.end)
+        ical_event.add('location', e.location)
+        ical_event.add('description', e.category + " " + str(e.url))
+        ical_event['uid'] = '%d.event.events.%s' % (e.id, site_token)
+        cal.add_component(ical_event)
+    
     response = HttpResponse(cal.to_ical(), mimetype="text/calendar")
-    response['Content-Disposition'] = 'attachment; filename=%s.ics' % "KOLEDAR"
-    print str(cal.to_ical())
+    response['Content-Disposition'] = 'attachment; filename=%s.ics' % "urnik"
     return response
