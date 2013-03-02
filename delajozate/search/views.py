6b47d7047a676982626cb125c3ee761938cb4147
Jernej Virag <jernej@virag.si>
2013-03-02 12:20:21 +0100

    Sorting of search results

--- delajozate/search/views.py
+++ delajozate/search/views.py
@@ -10,41 +10,42 @@ def index(request):
 	if request.GET.has_key('q'):
 		# Do search
 		solr = pysolarized.Solr(settings.SOLR_URL)
-		results = solr.query(request.GET.get('q'))
+		results = solr.query(request.GET.get('q'), sort=["tip asc", "datum_zapisa desc", "datum_od desc"])
 
-		for result in results.documents:
-			if result["tip"] == "oseba":
-				context["osebe"].append({"ime": result["ime"], "slug": result["str_slug"]})
-			elif result["tip"] == "stranka":
-				stranka = {"ime" : result["ime"],
-				            "okrajsava": result["str_okrajsava"] }
+		if not results or results.results_count == 0:
+			context["results"] = None
+		else:
+			for result in results.documents:
+				if result["tip"] == "oseba":
+					context["osebe"].append({"ime": result["ime"], "slug": result["str_slug"]})
+				elif result["tip"] == "stranka":
+					stranka = {"ime" : result["ime"],
+					            "okrajsava": result["str_okrajsava"] }
 
-				if "datum_od" in result:
-					stranka["od"] = pysolarized.from_solr_date(result["datum_od"])
-				if "datum_do" in result:
-					stranka["do"] = pysolarized.from_solr_date(result["datum_do"])
+					if "datum_od" in result:
+						stranka["od"] = pysolarized.from_solr_date(result["datum_od"])
+					if "datum_do" in result:
+						stranka["do"] = pysolarized.from_solr_date(result["datum_do"])
 
-				context["stranke"].append(stranka)
-				print result
-			elif result["tip"] == "zapis":
-				id = result["id"]
-				date = pysolarized.from_solr_date(result["datum_zapisa"])
-				zapis = {"ime_seje": result["str_ime_seje"],
-				         "datum": date,
-				         "permalink": result["str_permalink"],
-				         "seq": result["str_seq"], }
+					context["stranke"].append(stranka)
+				elif result["tip"] == "zapis":
+					id = result["id"]
+					date = pysolarized.from_solr_date(result["datum_zapisa"])
+					zapis = {"ime_seje": result["str_ime_seje"],
+					         "datum": date,
+					         "permalink": result["str_permalink"],
+					         "seq": result["str_seq"], }
 
-				if "id_oseba" in result:
-					govorec = Oseba.objects.get(pk=int(result["id_oseba"]))
-					zapis["ime_govorca"] = "%s %s" % (govorec.ime, govorec.priimek,)
-					zapis["govorec_slug"] = govorec.slug
+					if "id_oseba" in result:
+						zapis["ime_govorca"] = result["txt_govorec"]
+						zapis["govorec_slug"] = result["str_govorec_slug"]
 
-				if id in results.highlights:
-					zapis["vsebina"] = results.highlights[id].get("vsebina", None)
-				elif "vsebina" in result:
-					zapis["vsebina"] = result["vsebina"]
+					if id in results.highlights:
+						zapis["vsebina"] = results.highlights[id].get("vsebina", None)
+					elif "vsebina" in result:
+						zapis["vsebina"] = result["vsebina"]
 
-				context["zapisi"].append(zapis)
+					context["zapisi"].append(zapis)
 
 		context["results"] = results.documents
 
